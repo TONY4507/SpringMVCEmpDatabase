b6d6f5db4b9456ef7c8e75e0d7b3f7b416137ab9
Suprit Halgunaki <60099895+TONY4507@users.noreply.github.com>
2023-08-21 16:17:31 +0530

    Update EmpController.java

--- src/main/java/com/empdatabase/controllers/EmpController.java
+++ src/main/java/com/empdatabase/controllers/EmpController.java
@@ -12,46 +12,39 @@ import com.empdatabase.dao.EmpDao;
 @Controller  
 public class EmpController {  
     @Autowired  
-    EmpDao dao;//will inject dao from xml file  
-      
-    /*It displays a form to input data, here "command" is a reserved request attribute 
-     *which is used to display object data into form 
-     */  
+    EmpDao dao;
     @RequestMapping("/empform")  
     public String showform(Model m){  
     	m.addAttribute("command", new Emp());
     	return "empform"; 
     }  
-    /*It saves object into database. The @ModelAttribute puts request data 
-     *  into model object. You need to mention RequestMethod.POST method  
-     *  because default request is GET*/  
+ 
     @RequestMapping(value="/save",method = RequestMethod.POST)  
     public String save(@ModelAttribute("emp") Emp emp){  
         dao.save(emp);  
-        return "redirect:/viewemp";//will redirect to viewemp request mapping  
+        return "redirect:/viewemp";  
     }  
-    /* It provides list of employees in model object */  
+  
     @RequestMapping("/viewemp")  
     public String viewemp(Model m){  
         List<Emp> list=dao.getEmployees();  
         m.addAttribute("list",list);
         return "viewemp";  
     }  
-    /* It displays object data into form for the given id.  
-     * The @PathVariable puts URL data into variable.*/  
+  
     @RequestMapping(value="/editemp/{id}")  
     public String edit(@PathVariable int id, Model m){  
         Emp emp=dao.getEmpById(id);  
         m.addAttribute("command",emp);
         return "empeditform";  
     }  
-    /* It updates model object. */  
+   
     @RequestMapping(value="/editsave",method = RequestMethod.POST)  
     public String editsave(@ModelAttribute("emp") Emp emp){  
         dao.update(emp);  
         return "redirect:/viewemp";  
     }  
-    /* It deletes record for the given id in URL and redirects to /viewemp */  
+
     @RequestMapping(value="/deleteemp/{id}",method = RequestMethod.GET)  
     public String delete(@PathVariable int id){  
         dao.delete(id);  
